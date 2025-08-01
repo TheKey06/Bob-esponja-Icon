317948c9e5c9283573062fb8f5c500bd661743dc
keyner <keynerriv0606@gmail.com>
2025-08-01 17:15:09 -0500

    organizando la cara de bob esponja

--- Bases/src/Cuerpo.tsx
+++ Bases/src/Cuerpo.tsx
@@ -1,13 +1,39 @@
 export const Cuerpo = () => {
 
 return (<div className="Cuerpo">
-    <div className="Cabeza"></div>
+    <div className="Cabeza">
+        <div className="ojos" id="ojo_izquierdo">
+            <div className="iris">
+                <div className="pupila"></div>
+            </div>
+        </div>
+
+        <div className="ojos" id="ojo_derecho">
+            <div className="iris">
+                <div className="pupila"></div>
+            </div>
+        </div>
+        <div className="nariz"></div>
+        <div className="boca">
+            <div className="cachetes"></div>
+            <div className="cachetes"></div>
+            <div className="dientes" id="diente_derecho"></div>
+            <div className="dientes" id="diente_izquierdo"></div>
+        </div>
+        <div className="pecas" id="izquierda_superior"></div>
+        <div className="pecas" id="abajo_izquierda"></div>
+        <div className="pecas" id="derecha_superior"></div>
+        <div className="pecas" id="derecha_abajo_media"></div>
+        <div className="pecas" id="bottom_right"></div>
+    </div>
+
     <div className="ropa">
         <div className="Camisa">
             <div className="doble" id="DobleDerecho"></div>
             <div className="Corbata" id="CamisaCorbata"></div>
             <div className="doble" id="DobleIzquierdo"></div>
         </div>
+
         <div className="Pantalon">
             <div className="Correa"></div>
             <div className="Correa"></div>
